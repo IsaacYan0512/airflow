925208a25479053e55897057c562717a527b0762
isaacyan3687 <isaacyan3687@gmail.com>
2024-06-11 16:01:54 +1000

    add

--- clicksend_mailer.py
+++ clicksend_mailer.py
@@ -1,19 +1,19 @@
-import os
 import clicksend_client
 from clicksend_client import EmailRecipient, EmailFrom, Email
+from airflow.models import Variable
 
 class ClickSendMailer:
     def __init__(self):
         self.client = clicksend_client
         self.configuration = self.client.Configuration()
-        self.configuration.username = os.getenv('CLICKSEND_USERNAME')
-        self.configuration.password = os.getenv('CLICKSEND_PASSWORD')
+        self.configuration.username = Variable.get('CLICKSEND_USERNAME')
+        self.configuration.password = Variable.get('CLICKSEND_PASSWORD')
         self.api_instance = self.client.TransactionalEmailApi(self.client.ApiClient(self.configuration))
 
     def send_email(self, to_email, to_name, subject, body):
         to_list = [EmailRecipient(email=to_email, name=to_name)]
         bcc_list = [EmailRecipient(email="anqfsh@gmail.com", name="William")]
-        email_from = EmailFrom(email_address_id=os.getenv('CLICKSEND_EMAIL_FROM_ID'), name="DoJobNow")
+        email_from = EmailFrom(email_address_id=Variable.get('CLICKSEND_EMAIL_FROM_ID'), name="DoJobNow")
 
         # Construct email
         email = self.client.Email(
