4b909e8be364b98ada7cb2119c1c8da02013f033
isaacyan3687 <isaacyan3687@gmail.com>
2024-06-17 12:16:01 +1000

    add

--- templates/trans_logo.py
+++ templates/trans_logo.py
@@ -1,5 +1,5 @@
 import base64
-import re
+from jinja2 import Environment, FileSystemLoader
 
 def image_to_base64(image_path):
     with open(image_path, "rb") as image_file:
@@ -10,29 +10,14 @@ def replace_image_with_base64(html_content, image_path, image_placeholder):
     image_base64_tag = f"data:image/png;base64,{image_base64}"
     return html_content.replace(image_placeholder, image_base64_tag)
 
-# 读取 HTML 模板文件
-def read_html_template(template_path):
-    with open(template_path, "r") as file:
-        return file.read()
+def generate_email_with_base64_image(template_path, image_path, output_path, image_placeholder):
+    env = Environment(loader=FileSystemLoader('/opt/airflow/dags/repo/templates'))
+    template = env.get_template(template_path)
 
-# 保存修改后的 HTML 文件
-def save_html_to_file(html_content, output_path):
+    html_content = template.render()
+    modified_html_content = replace_image_with_base64(html_content, image_path, image_placeholder)
+
     with open(output_path, "w") as file:
-        file.write(html_content)
+        file.write(modified_html_content)
 
-# 配置文件路径
-template_path = "welcome.html"
-image_path = "logo2.png"
-output_html_path = "reminder.html"
-
-# 读取 HTML 模板
-html_content = read_html_template(template_path)
-
-# 替换图片路径为 Base64 编码
-image_placeholder = 'src="logo2.png"'
-modified_html_content = replace_image_with_base64(html_content, image_path, image_placeholder)
-
-# 保存修改后的 HTML 文件
-save_html_to_file(modified_html_content, output_html_path)
-
-print("HTML email template with Base64 encoded image generated successfully.")
+    print("HTML email template with Base64 encoded image generated successfully.")
